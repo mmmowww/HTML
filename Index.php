6c3ebbe37743f64e9478dcbcdad04c1999c3a026
mmmowww <36041208+mmmowww@users.noreply.github.com>
2018-02-01 14:53:59 +0000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Эйс Ти</title>
+</head>
+<style>
+body {background-color: Black;}
+</style>
+<body style="color:green;">
+<div id="container">
+<nav id="topnav">
+<a href="Index.php" title="Главаня"><img src="G.jpg" alt="Главная"/></a>
+<a href="about.html" title="Я о себе"><img src="I.jpg" alt="Я о себе"/></a>
+<a href="New.html" title="Новости"><img src="N.jpg" alt="Новости"/></a>
+<a href="photo.html" title="Фото"><img src="M.jpg" alt="Мои фото"/></a>  
+<a href="Contakt.html" title="Контакты"><img src="K.jpg" alt="Контакты"/></a>
+<a href="download.html" title="Скачать"><img src="MM.jpg" alt="Скачать музыку"/></a>
+<a href="video.html" title="Видео"><img src="V.jpg" alt="Видео"/></a>
+</nav>
+<div id="container">
+<img src="main.jpg"
+ alt ="Моя фотография"
+ heigh ="400" width="400"/>
+<h1> К вашим услугам музыкант <h1>
+<h2> Певец, клавешник и просто хорошый парень! <h2>
+<h2> Этот сайт я посвещаю себе и своему творчесву <h2>
+<h2> Тут я буду размещать свою музыку, тексты песен и то что сочту нужным <h2>
+
+<?php
+define("PI",3.14); // 
+define("Pi",3.14); // 
+define("pi",3.14); // 
+
+
+
+//echo PHP_VERSION;
+
+
+?>
+<h5>Версия сайта 1.1.01<h5>
+<h5>Дата релиза 17.05.2017<h5>
+<h5> Автор Арбатов В.В.<h5>
+</body>
+</html>
